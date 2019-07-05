c62378da7aadeceeadd6cdde7738cc68c3208122
yuulye <yuulye@gmail.com>
2019-07-05 20:19:45 +0700

    add firebase auth

---
+++ src/firebaseConfig.js
@@ -0,0 +1,11 @@
+const config = {
+  apiKey: "AIzaSyCC5hLus2-XzkS9-UGFKsaGow4Is3K01O0",
+  authDomain: "lyeyuu.firebaseapp.com",
+  databaseURL: "https://lyeyuu.firebaseio.com",
+  projectId: "lyeyuu",
+  storageBucket: "",
+  messagingSenderId: "1073685159978",
+  appId: "1:1073685159978:web:fd4546c3e9f6ab2c"
+};
+
+export default config;
